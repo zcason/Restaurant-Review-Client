4ab51a0e7748ceda12578806058c218a4421ca26
zcason <zuri.cason@gmail.com>
2021-06-09 13:38:44 -0500

    changed api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: 'https://still-forest-75414.herokuapp.com'
+    API_ENDPOINT: 'https://dry-river-65869.herokuapp.com'
 };
